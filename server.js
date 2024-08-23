88158f49d3f62449fd84e3f8b7f956bb1be672c8
swastik pradhan <b421061@iiit-bh.ac.in>
2024-08-23 23:20:42 +0530

    UPDATE CORS

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions)); // Use CORS with the specified options
+app.options('*', cors(corsOptions)); // Pre-flight handling for all routes
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
