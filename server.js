f9dfa022a143d259dc2965da856f05d164ef5805
swastik pradhan <b421061@iiit-bh.ac.in>
2024-08-23 23:32:24 +0530

    UPDATE CORS

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use("/api/auth");
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
