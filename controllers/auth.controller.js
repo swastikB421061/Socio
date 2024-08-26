608bf1ef10b07ee9b371978e05341fc522ab9c46
swastik pradhan <b421061@iiit-bh.ac.in>
2024-08-26 20:29:42 +0530

    UPDATE little

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -99,6 +99,7 @@ export const logout = async (req, res) => {
 export const getMe = async (req, res) => {
 	try {
 		const user = await User.findById(req.user._id).select("-password");
+		console.log(user);
 		res.status(200).json(user);
 	} catch (error) {
 		console.log("Error in getMe controller", error.message);
