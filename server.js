c4c7002750c1bab4860023f98a567ed46c8e4b87
swastik pradhan <b421061@iiit-bh.ac.in>
2024-08-26 20:45:19 +0530

    parser

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"; // Import CORS
+import cors from "cors"; 
+import bodyParser from "body-parser";
 import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.route.js";
@@ -35,7 +36,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions)); // Use CORS with the specified options
 app.options('*', cors(corsOptions)); // Pre-flight handling for all routes
-
+app.use(bodyParser.json());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
